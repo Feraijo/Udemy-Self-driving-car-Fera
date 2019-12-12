d66c1f0a5622c8d34f6eeefb2d7e39556a6f2d3a
Ilia Gurov <feraijo@gmail.com>
2019-12-12 16:43:34 +0500

    Hough alg added

--- finding_lanes/lanes.py
+++ finding_lanes/lanes.py
@@ -6,6 +6,31 @@ import os
 filepath = os.path.dirname(os.path.relpath(__file__))
 image = cv2.imread(filepath+'/Image/test_image.jpg')
 
+def make_coords(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope, intercept = parameters
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    left_fit_avg = np.average(left_fit, axis=0)
+    right_fit_avg = np.average(right_fit, axis=0)
+    left_line = make_coords(image, left_fit_avg)
+    right_line = make_coords(image, right_fit_avg)
+    return np.array([left_line, right_line])
+
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -15,11 +40,9 @@ def canny(image):
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(line_image,
-                     (x1, y1), (x2, y2),
-                     (255, 0, 0), 10)
+        for x1, y1, x2, y2 in lines:            
+            cv2.line(line_image, (x1, y1), 
+                    (x2, y2), (255, 0, 0), 10)
     return line_image
 
 def region_of_interest(image):
@@ -41,8 +64,9 @@ lines = cv2.HoughLinesP(cropped, 2,
                         np.array([]),
                         minLineLength=40,
                         maxLineGap=5)
-
-line_image = display_lines(lane_image, lines)
+                        
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
 combo_image = cv2.addWeighted(lane_image, 0.8,
                               line_image, 1, 1)
 #plt.imshow(c)
